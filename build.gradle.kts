108624f974b819313bfa5a2f3a3b5010e89605ce
kevin.perez5 <kevin.perez@naranjax.com>
2025-06-08 13:37:21 -0500

    jfrog config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,38 @@
+import org.jfrog.gradle.plugin.artifactory.dsl.ArtifactoryPluginConvention
+
 plugins {
+    alias(libs.plugins.jfrog.artifactory) version libs.versions.jfrogArtifactoryVersion.get() apply true
     `maven-publish`
 }
 
 allprojects {
-    group = "com.llamasoft.elessa"
+    group = "com.llamasoft.elessa.buildlogic"
     version = "0.0.1"
+
+    apply(plugin = "com.jfrog.artifactory")
+    apply(plugin = "maven-publish")
+}
+
+subprojects {
+    configure<ArtifactoryPluginConvention> {
+        publish {
+            contextUrl = "https://cb4d-179-6-6-210.ngrok-free.app/artifactory"
+            repository {
+                repoKey = "libs-release-local"
+                username = "admin"
+                password = "P7WxdLiZKSWD2yf"
+            }
+            defaults {
+                publications("maven")
+                setPublishArtifacts(true)
+                setPublishPom(true)
+            }
+        }
+    }
 }
 
 tasks.register("publishAll") {
-    dependsOn(":convention:publishAllPublicationsToArtifactoryRepository")
+    dependsOn(":convention:artifactoryPublish")
     group = "publishing"
     description = "Publish all modules to Artifactory"
 }
--- convention/build.gradle.kts
+++ convention/build.gradle.kts
@@ -67,40 +67,9 @@ publishing {
     publications {
         create<MavenPublication>("maven") {
             from(components["java"])
-
-            pom {
-                name.set("Elessa Build Logic")
-                description.set("Shared build logic for Elessa Android projects")
-                url.set("https://github.com/llamasoft/elessa-build-logic")
-
-                licenses {
-                    license {
-                        name.set("MIT License")
-                        url.set("https://opensource.org/licenses/MIT")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("llamasoft")
-                        name.set("Llama Software")
-                        email.set("dev@llamasoft.com")
-                    }
-                }
-            }
-        }
-    }
-
-    repositories {
-        maven {
-            name = "Artifactory"
-            url = uri("https://your-company.jfrog.io/artifactory/gradle-plugins")
-            credentials {
-                username = project.findProperty("artifactory.username") as String?
-                    ?: System.getenv("ARTIFACTORY_USERNAME")
-                password = project.findProperty("artifactory.password") as String?
-                    ?: System.getenv("ARTIFACTORY_PASSWORD")
-            }
+            groupId = "com.llamasoft.elessa.buildlogic"
+            artifactId = "convention"
+            version = "0.0.1"
         }
     }
 }
